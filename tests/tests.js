14fa93f3738149e29a7b7eb890c28bad9b67dec7
Alejandro Ravelo <alu0100763379@ull.edu.es>
2015-10-15 18:50:32 +0100

    añadidas pruebas

--- tests/tests.js
+++ tests/tests.js
@@ -10,12 +10,12 @@ describe("Temperature_Tests", function() {
     it("67F", function() {
         var temp = new Temperatura(67,"F");
         expect(temp.num).to.equal(67);
-        expect(temp.num).to.equal("F");
+        expect(temp.tipo).to.equal("F");
     });
     it("273.1K", function() {
         var temp = new Temperatura(273.1,"K");
-        expect(temp.get_valor()).to.equal(273.1);
-        expect(temp.get_tipo()).to.equal("K");
+        expect(temp.num).to.equal(273.1);
+        expect(temp.tipo).to.equal("K");
     });
     /*it('32F = 0C', function() {
         original.value = "32F";
