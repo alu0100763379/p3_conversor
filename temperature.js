adfb8801cd5dc19261222cd9c94bca8c38b970c3
Alejandro Ravelo <alu0100763379@ull.edu.es>
2015-10-20 13:54:36 +0100

    implementadas las funciones y solucionados los errores

--- temperature.js
+++ temperature.js
@@ -1,78 +1,95 @@
-"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
-
-function Medida (num, tipo) {
-  this.num = num;
-  this.tipo = tipo;
+"use strict";
+function Medida(num, tipo){
+  this.num_=num;
+  this.tipo_=tipo;
 }
 
-function Temperatura (num, tipo) {
-  Medida.call(this, num, tipo);
+function Temperatura (num, tipo){
+   Medida.call(this, num, tipo);
 }
 
 Temperatura.prototype = new Medida();
 
-function calculate() {
-  var result1 = new Temperatura();
-  var result2 = new Temperatura();
-  var temp = original.value; //almacena en el, la variable temp el valor original.
-  var conversor = /(^[-+]?\d+(?:\.\d*)?)(?:[eE]?([-+]?\d+))?\s*([fFcCkK])/;
-  var m = temp.match(conversor);
-  if (m) {
-    var med = new Temperatura()
-    med.num = m[1];
-    med.tipo = m[3];
-    if (m[2] !== undefined){
-      var exp = m[2];
-      exp = parseInt(exp); // Convierte una string en entero
+Medida.prototype.get_num = function(){ 
+  return this.num_; 
+}
 
-      if (exp<0){
-        exp = -exp; 
-        var cnt = 1, divide = 10;
+Medida.prototype.get_tipo = function(){ 
+  return this.tipo_; 
+}
 
-        while(cnt < exp){
-          divide = divide * 10;
-          cnt++; 
+Medida.prototype.set_num = function(num){ 
+  this.num_= num; 
+}
+
+Medida.prototype.set_tipo = function(tipo){ 
+  this.tipo_= tipo; 
+}
+
+Temperatura.prototype.convert_celsius_to_farenheit = function(){ 
+  return (this.get_num() * 9/5) + 32; 
+}
+
+Temperatura.prototype.convert_farenheit_to_celsius = function(){ 
+  return (this.get_num() - 32) * 5/9; 
+}
+
+Temperatura.prototype.convert_farenheit_to_kelvin = function(){ 
+  return ((this.get_num() - 32) * 5/9) + 273.15; 
+}
+
+Temperatura.prototype.convert_celsius_to_kelvin = function(){ 
+  return (this.get_num() + 273.15); 
+}
+
+Temperatura.prototype.convert_kelvin_to_farenheit = function(){ 
+  return (this.get_num() - 273.15) * 1.8 + 32; 
+}
+
+Temperatura.prototype.convert_kelvin_to_celsius = function(){ 
+  return (this.get_num() - 273.15); 
+}
+
+function calculate(){
+    var result1 = new Temperatura();
+    var result2 = new Temperatura();
+    var temp = original.value;
+
+    if(temp){
+      var regexp = /([-+]?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)\s*([fFcCkK])/;
+      var m = temp.match(regexp);
+
+      if(m){
+        var cnv_tmp = new Temperatura();
+
+        cnv_tmp.set_num(parseFloat(m[1]));
+        cnv_tmp.set_tipo(m[2]);
+
+        if (cnv_tmp.get_tipo() == 'c' || cnv_tmp.get_tipo() == 'C') {
+          result1.set_num(cnv_tmp.convert_celsius_to_farenheit());
+          result1.set_tipo("Fahrenheit");
+          result2.set_num(cnv_tmp.convert_celsius_to_kelvin());
+          result2.set_tipo("Kelvin");
         }
-
-        if(divide !== 0) med.num = med.num/divide;
-
-      } 
+        if (cnv_tmp.get_tipo() == 'f' || cnv_tmp.get_tipo() == 'F') {
+          result1.set_num(cnv_tmp.convert_farenheit_to_celsius());
+          result1.set_tipo("Celsius");
+          result2.set_num(cnv_tmp.convert_farenheit_to_kelvin());
+          result2.set_tipo("Kelvin");
+        }
+        if (cnv_tmp.get_tipo() == 'k' || cnv_tmp.get_tipo() == 'K') {
+          result1.set_num(cnv_tmp.convert_kelvin_to_celsius());
+          result1.set_tipo("Celsius");
+          result2.set_num(cnv_tmp.convert_kelvin_to_farenheit());
+          result2.set_tipo("Fahrenheit");
+        }
+        
+        result1 = result1.get_num() + " " + result1.get_tipo();
+        result2 = result2.get_num() + " " + result2.get_tipo();
+        converted.innerHTML = result1 + " || " + result2;
+      }
       else {
-        var cnt = 1, divide = 10;
-
-        while(cnt < exp){
-          divide = divide * 10;
-          cnt++; 
-        }
-        if(divide !== 0) med.num = med.num*divide; 
+        converted.innerHTML = "ERROR! Intenta poner algo como: '-4.2C'";
       }
     }
-    if (med.tipo == 'c' || med.tipo == 'C') {
-      result1.num = (med.num * 9/5)+32;
-      result1.num = result1.num.toFixed(1)+" Farenheit";
-      result2.num = (med.num*1)+273.15;
-      result2.num = result2.num.toFixed(1)+" Kelvin";
-      
-    }
-    else if (med.tipo == 'f' || med.tipo == 'F'){
-      result1.num = (med.num - 32)*5/9;
-      result1.num = result1.num.toFixed(1)+" Celsius";
-      result2.num = ((med.num - 32)*5/9) + 273.15;
-      result2.num = result2.num.toFixed(1)+" Kelvin";
-    }
-    else{
-      result1.num = med.num - 273.15;
-      result1.num = result1.num.toFixed(1)+" Celsius";
-      result2.num = (med.num * 9/5) - 459.67;
-      result2.num = result2.num.toFixed(1)+" Farenheit";
-    }
-    converted1.innerHTML = result1.num;
-    converted2.innerHTML = result2.num;
-    converted.innerHTML = "";
-  }
-  else {
-    converted.innerHTML = "ERROR! Intenta poner algo como:'-4.2C'";  //no es una entrada valida
-    converted1.innerHTML = "";
-    converted2.innerHTML = "";
-  }
 }
